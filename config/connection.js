4a077cfd48705dbac68f71adc892785bcbfdee79
Lauryn McFarland <lauryn@lauryns-mbp.attlocal.net>
2018-02-17 22:12:47 -0600

    added JAWSDB to connections

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "horsesDB"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "horsesDB"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
